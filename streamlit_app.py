c17c65f1b1dd311f92359e40b544b2c4f87d4f9c
epatyukova <patyukova@gmail.com>
2024-12-27 11:23:06 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+
+with st.sidebar:
+    number_of_elements = st.selectbox('Number of elements', 
+                              ('2','3','4'), 
+                              index=None, 
+                              placeholder='3')
+
+st.title("🎈 Disorder in crystals")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
